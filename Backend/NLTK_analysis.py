027f110ebb874b99c24fe680addcf29503d1681d
Shepard Jiang <shepardjiang2025@gmail.com>
2023-02-02 21:17:00 -0500

    Add NLTK sentiment analyzer for headlines.

--- Backend/NLTK_analysis.py
+++ Backend/NLTK_analysis.py
@@ -1,15 +1,33 @@
 from nltk.sentiment import SentimentIntensityAnalyzer
+import datetime
 import sqlite3
+from scrapers.crawler_settings import Settings
 
 con = sqlite3.connect("ticker_info.db")
 cur = con.cursor()
-
-res = cur.execute("SELECT headline FROM all_headlines where \
-    ticker='META' and source='wsj' and queryDate='2023-01-10'")
-
+today = datetime.date.today()
+dates = [today.isoformat(), 
+        (today - datetime.timedelta(days=1)).isoformat(), 
+        (today - datetime.timedelta(days=2)).isoformat()]
 sia = SentimentIntensityAnalyzer()
-run_sum = 0
-headlines = res.fetchall()
-for headline in headlines:
-    run_sum += sia.polarity_scores(headline[0])['compound']
-print(run_sum/len(headlines))
+
+for company, ticker in Settings.get_company_tickers():
+    all_sum = 0
+    for source in Settings.get_sources():
+        sub_sum = 0
+        sub_count = 0
+
+        for date in dates:
+            query = f"SELECT headline FROM all_headlines WHERE ticker='{ticker}' and source='{source}' and queryDate='{date}'"
+            res = cur.execute(query)
+            headlines = res.fetchall()
+            sub_count += len(headlines)
+            for headline in headlines:
+                sub_sum += sia.polarity_scores(headline[0])['compound']
+        if sub_count:
+            cur.execute("INSERT INTO SCORES VALUES(?, ?, ?, ?)", (today, ticker, source, sub_sum/sub_count))
+            all_sum += (sub_sum/sub_count)
+    cur.execute("INSERT INTO SCORES VALUES(?, ?, ?, ?)", (today, ticker, 'ALL', all_sum/len(Settings.get_sources())))
+
+con.commit()
+con.close()
