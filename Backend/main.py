c786ce42183725bf8e4c97a3471aa209d1fd6e85
Shepard Jiang <shepardjiang2025@gmail.com>
2023-02-02 21:18:49 -0500

    Add API functionality to Flask backend.

--- Backend/main.py
+++ Backend/main.py
@@ -3,8 +3,11 @@ from flask_sqlalchemy import SQLAlchemy
 import datetime
 import sqlite3
 import os
+from scrapers.crawler_settings import Settings
 
 app = Flask(__name__)
+
+today = datetime.date.today().isoformat()
 
 # app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///ticker_info.db"
 # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -22,27 +25,35 @@ app = Flask(__name__)
 #         self.tckr = tckr
 #         self.score = score
 
-
-@app.route("/")
-def index():
-    con = sqlite3.connect("./Backend/ticker_info.db")
+@app.route("/api/<string:date>", methods=['GET'])
+def all_scores(date):
+    con = sqlite3.connect("ticker_info.db")
     cur = con.cursor()
-    res = cur.execute("SELECT * FROM all_headlines WHERE ticker='AMZN'")
-    # print()
-    # return render_template("index.html", token="Hello Flask + React")
-    # con.close()
-    return {"hi": res.fetchone(), "hi2": "'"}
+    data = {}
+    for source in Settings.get_sources() + ['ALL']:
+        res = cur.execute(f"SELECT TICKER, SCORE FROM SCORES WHERE SOURCE='{source}' AND DATE='{date}'")
+        source_data = {ticker: score for ticker, score in res.fetchall()}
+        data[source] = source_data 
+    return data
 
-@app.route("/scores")
-def members():
-    return {'tesla': 0.49, 'amazon': 0.2, 'google': 0.35, 'microsoft': 0.23}
 
-@app.route("/<int:celsius>")
-def fahrenheit_from(celsius):
-    """Convert Celsius to Fahrenheit degrees."""
-    fahrenheit = float(celsius) * 9 / 5 + 32
-    fahrenheit = round(fahrenheit, 3)  # Round to three decimal places
-    return str(fahrenheit)
+@app.route("/api/<string:date>/scores/<string:ticker>/<string:source>", methods=['GET'])
+def ticker_score(ticker, source, date):
+    con = sqlite3.connect("ticker_info.db")
+    cur = con.cursor()
+    res = cur.execute(f"SELECT SCORE FROM SCORES WHERE TICKER='{ticker}' AND SOURCE='{source}' AND DATE='{date}'")
+    score = res.fetchone()
+    con.close()
+    if score:
+        return {'score': score[0]}
+    else:
+        return {'score': None}
+
+
+@app.route("/api/<string:date>/headlines/<string:ticker>")
+def headlines(ticker):
+    # WIP
+    pass
 
 if __name__ == "__main__":
     app.run(port=8080, debug=True)
